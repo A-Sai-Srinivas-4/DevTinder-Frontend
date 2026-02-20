71acd63bfe6e23fbbf50ea80539cd6cc7a7502ac
A-Sai-Srinivas-4 <asaisrinivas4@gmail.com>
2026-02-20 21:07:10 +0530

    added axios lib for api calling and fixed cors issue with adding withCredentials is true in login api.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,61 @@
+import axios from "axios";
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [emailId, setEmailId] = useState("saisrinivas@gmail.com");
+  const [password, setPassword] = useState("Sai@1234");
 
-export default Login
+  const handleEmailChange = (e) => setEmailId(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:3003/api/login",
+        {
+          emailId,
+          password,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+
+      console.log("res==========>", res);
+    } catch (error) {
+      console.log("Error logging in:", error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input
+          type="email"
+          value={emailId}
+          onChange={handleEmailChange}
+          className="input"
+          placeholder="Email"
+        />
+
+        <label className="label">Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          className="input"
+          placeholder="Password"
+        />
+
+        <button className="btn btn-neutral mt-4" onClick={handleLogin}>
+          Login
+        </button>
+      </fieldset>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/services/api/apiClient.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+export const apiClient = axios.create({
+    baseURL: "https://api.themoviedb.org/3",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});

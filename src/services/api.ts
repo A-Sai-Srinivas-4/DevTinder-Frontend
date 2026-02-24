8b0958375018b3c507983b55213d10c5e9fa70c3
A-Sai-Srinivas-4 <asaisrinivas4@gmail.com>
2026-02-24 17:29:56 +0530

    updated VITE_API_KEY for nginx setup

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,7 +8,7 @@ interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
 }
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3003/api",
+  baseURL: import.meta.env.VITE_API_URL || "/api",
   withCredentials: true,
 });
 
